0a91076751f9d396e9e9d86decb67327892c0359
mirkhamroev <xamrayevmironshoh4@gmail.com>
2026-09-23 21:31:25 +0500

    new static media upload part has been added to urlpatterns

--- core/urls.py
+++ core/urls.py
@@ -20,6 +20,8 @@ from apps.accounts.urls import urlpatterns as accounts_urls
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+import core.settings as settings
+from django.conf.urls.static import static
 
 
 schema_view = get_schema_view(
@@ -41,4 +43,4 @@ urlpatterns = [
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('admin/', admin.site.urls),
     path('api/accounts/', include(accounts_urls))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
